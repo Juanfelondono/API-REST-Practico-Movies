dfd0dd596e7f72ff1f027c8b934b3e011d695afd
Juanfelondono <felipe.londono@utp.edu.co>
2023-01-15 12:59:42 -0500

    Lista de categorias

--- src/main.js
+++ src/main.js
@@ -16,8 +16,31 @@ async function getTrendingMoviesPreview() {
         // aca los empiezo a adicionar
         movieContainer.appendChild(movieImg);  
         trendingPreviewMovieContainer.appendChild(movieContainer); 
-    });
-    
+    });   
+}
 
+async function getCategoriesPreview() {
+    const res = await fetch ('https://api.themoviedb.org/3/genre/movie/list?api_key='+ API_KEY);
+    const data = await res.json(); // en data me quedan guardados todos los valores de la repsuesta de la api
+
+    const categories = data.genres;
+    categories.forEach(category => { // asi hago que cree esto por cada pelicula que recorre, revio el html para saber que debo crear
+        
+        const categoriesPreviewContainer = document.querySelector('#categoriesPreview .categoriesPreview-list') // esto lo realizo para poder seleccionar al articule donde esta guardado el div 
+        
+        const categoryContainer = document.createElement('div'); //creo un div para almacenar
+        categoryContainer.classList.add('category-container'); //hago que le agregue la clase al div
+
+        const categoryTitle = document.createElement('h3'); // creo el titulo h3
+        categoryTitle.classList.add('category-title'); // creo la clase de al titulo
+        categoryTitle.setAttribute('id', 'id'+ category.id); // Primero se pone el atributo que deseo y luego el valor
+        const categoryTitleText = document.createTextNode(category.name);
+
+        // aca los empiezo a adicionar
+        categoryTitle.appendChild(categoryTitleText); //agrego al h3 el titulo que deseo
+        categoryContainer.appendChild(categoryTitle); // agrego el h3 al div
+        categoriesPreviewContainer.appendChild(categoryContainer); // agrego el div al artcule
+    });   
 }
 getTrendingMoviesPreview();     
+getCategoriesPreview();
