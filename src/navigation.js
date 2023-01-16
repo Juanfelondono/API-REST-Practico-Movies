0c520c8a3d1ab8a8178372d13c09ffc1e3902bce
Juanfelondono <felipe.londono@utp.edu.co>
2023-01-15 23:42:19 -0500

    Creacion Navigator

---
+++ src/navigation.js
@@ -0,0 +1,46 @@
+
+window.addEventListener('DOMContentLoaded', navigator, false);
+window.addEventListener('hashchange', navigator, false);
+;
+
+function navigator () {
+    console.log({location});
+
+    if ( location.hash.startsWith('#trends')) {
+        trendsPage();
+    } else if (location.hash.startsWith('#search=')) {
+        searchPage();
+    } else if (location.hash.startsWith('#movie=')) {
+        movieDetailsPage();
+    } else if (location.hash.startsWith('#category=')) {
+        ccategoriesPage();
+    } else {
+        homePage();
+    }
+    location.hash
+}
+
+function homePage() {
+    console.log ('Home!!')
+    getTrendingMoviesPreview();   // las hago cargar solo cuando hash esta en el home  
+    getCategoriesPreview();
+}
+
+function trendsPage() {
+    console.log ('TRENDS!!')
+}
+
+function searchPage() {
+    console.log ('Search!!')
+}
+
+function movieDetailsPage() {
+    console.log ('Movie!!')
+}
+
+function categoriesPage() {
+    console.log ('categories!!')
+}
+
+
+
